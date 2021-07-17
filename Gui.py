4e428a6e95b67a022f871a96405367d011307268
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-17 16:40:49 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -1,9 +1,12 @@
 #need pyqt : pip3 install pyqt5
+
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+
 app = QApplication(sys.argv) #create an instance of QApplication
+
 #Create an instance of the app gui
 window = QWidget()
 window.setWindowTitle('Youtube Downloader Py')
@@ -11,3 +14,5 @@ window.setGeometry(100, 100, 280, 80)
 window.move(60, 15)
 helloMsg = QLabel('<h1>Welcome in Youtube Downloader Py!</h1>', parent=window)
 helloMsg.move(60, 15)
+
+window.show()
