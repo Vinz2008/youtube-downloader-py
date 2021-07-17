b13f91bebd3eaae30a9716ad2f0f0516011795ff
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-17 17:01:46 +0200

    Update Gui2.py

--- Gui2.py
+++ Gui2.py
@@ -1,1 +1,5 @@
-
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+app = QApplication(sys.argv) # Pass in sys.argv to allow command line arguments for your app
+window = QWidget() #create a window
+window.show() #show the window
