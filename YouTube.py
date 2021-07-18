5c3cf3256d126d1c1a95f48193a3fa57b9e3e841
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-18 21:56:38 +0200

    Update YouTube.py

--- YouTube.py
+++ YouTube.py
@@ -1,8 +1,21 @@
 #need to install pytube to use the script : pip3 install pytube
 import pytube
 from pytube import YouTube
+
+def progress_function(self,stream, chunk,file_handle, bytes_remaining):
+  size = stream.filesize
+  p = 0
+  while p <= 100:
+    progress = p
+    print("%i %%" % progress)
+    p = percent(bytes_remaining, size)
+
+def percent(self, tem, total):
+        perc = (float(tem) / float(total)) * float(100)
+        return perc
+    
 link = input('Enter the link: ') #asking the link 
-yt = YouTube(link) 
+yt = YouTube(link, on_progress_callback=progress_function)
 VideoOrAudio = input("Are you downloading  1.a video or 2.an audio (enter 1 or 2): ") #asking if you are downloading an audio or a video
 #resolution = input("Enter the resolution") #asking the resolution
 if VideoOrAudio == "1":
@@ -16,10 +29,12 @@ if VideoOrAudio == "2":
   stream = yt.streams.get_by_itag(22)
   stream.download()
  
-size = stream.filesize
-p = 0
-while p <= 100:
-  progress = p
-  print("%i %%" % progress)
-  h = bytes_remaining/size
-  p = h * 100
+def progress_function(self,stream, chunk,file_handle, bytes_remaining):
+  
+  size = stream.filesize
+  p = 0
+  while p <= 100:
+    progress = p
+    print("%i %%" % progress)
+    h = bytes_remaining/size
+    p = h * 100
