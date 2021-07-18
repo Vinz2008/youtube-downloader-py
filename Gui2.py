2b1c774e957cd2bd39ec29669878fec0012960cc
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-18 14:34:36 +0200

    Update Gui2.py

--- Gui2.py
+++ Gui2.py
@@ -1,5 +1,21 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 import sys
-app = QApplication(sys.argv) # Pass in sys.argv to allow command line arguments for your app
-window = QWidget() #create a window
+
+#Subclass QMainWindow to customize the main window
+class MainWindow(QMainWindow);
+  def _init_(self):
+    super()._init_()
+    
+    self.setWindowTitle("Youtube Downloader Py") #assign a name to the window
+    button = QPushButton("Download") #create a button
+    
+    self.setCentralWidget(button)
+    
+app = QApplication(sys.argv) # Pass in sys.argv to allow command line arguments
+
+
+window = QMainWindow() #create a window and say what there is in it
 window.show() #show the window
+
+
+app.exec() #start the event loop
