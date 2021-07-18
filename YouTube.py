7154aa41898ef15bb5de7f93b1b6a1291bb2857f
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-18 21:11:16 +0200

    Update YouTube.py

--- YouTube.py
+++ YouTube.py
@@ -4,10 +4,10 @@ from pytube import YouTube
 link = input('Enter the link: ') #asking the link 
 yt = YouTube(link) 
 VideoOrAudio = input("Are you downloading  1.a video or 2.an audio (enter 1 or 2)") #asking if you are downloading an audio or a video
-resolution = input("Enter the resolution") #asking the resolution
+#resolution = input("Enter the resolution") #asking the resolution
 if VideoOrAudio == "1":
-  yt.streams.filter(res=resolution , subtype = "mp4").first().download()
+  yt.streams.filter(res='1080' , subtype = "mp4").first().download()
 
 if VideoOrAudio == "2":
-  yt.streams.filter(res=resolution , subtype = "mp3").first().download()
+  yt.streams.filter(res='1080' , subtype = "mp3").first().download()
   
