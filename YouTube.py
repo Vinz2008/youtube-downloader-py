7d741b55b1c80b19a78c299e99f21eeb4ad732e5
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-18 02:22:30 +0200

    Update YouTube.py

--- YouTube.py
+++ YouTube.py
@@ -1,7 +1,7 @@
 #need to install pytube to use the script : pip3 install pytube
 import pytube
 from pytube import YouTube
-link = input(“Enter the link: “) #asking the link 
+link = input('Enter the link: ') #asking the link 
 yt = YouTube(link) 
 VideoOrAudio = input("Are you downloading  1.a video or 2.an audio (enter 1 or 2)") #asking if you are downloading an audio or a video
 resolution = input("Enter the resolution") #asking the resolution
