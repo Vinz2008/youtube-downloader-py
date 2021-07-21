696a1b44e726d784d7367f4b18e00bec2a26a731
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-21 02:14:10 +0200

    Update pysimplegui.py

--- pysimplegui.py
+++ pysimplegui.py
@@ -1,3 +1,8 @@
 #need PySimpleGui to run
 import PySimpleGUI as sg 
 sg.theme('SystemDefault') 
+layout = [[sg.Text('Welcome in youtube-downloader.py:'),
+           sg.Text(size=(15,1), key='-OUTPUT-')],
+          [sg.Input(key='-IN-')],
+          [sg.Button('Download'), sg.Button('Exit')]]
+  
