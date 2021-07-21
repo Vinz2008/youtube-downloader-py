1218917b8ffe61df99345594667f1cc0e3ada8d3
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-21 12:18:26 +0200

    Update pysimplegui.py

--- pysimplegui.py
+++ pysimplegui.py
@@ -9,12 +9,12 @@ sg.theme('SystemDefault')
 layout = [[sg.Text('Welcome in youtube-downloader.py:'),
            sg.Text(size=(15,1))],
           [sg.Text('Enter the url of the file you want to download:'),
-           sg.Text(size=(15,1))]
+           sg.Text(size=(15,1))],
           [sg.Input(key='Url')],
           [sg.Text('Enter if you you want from where to where you want the video to be'),
-           sg.Text(size=(15,1))]
-          [sg.Text('Start), sg.Input(key="Start"), sg.Text("End"), sg.Input(key="End")]
-          [sg.Button("Download"), sg.Button("Exit")]]
+           sg.Text(size=(15,1))],
+          [sg.Text('Start), sg.Input(key="Start"), sg.Text("End"), sg.Input(key="End")],
+          [sg.Button("Download"), sg.Button("Exit")]],
 
 window = sg.Window('Youtube downloader py', layout)
 event, values = window.read() #needed for doing events with things in the layout ex: if a button is clicked
