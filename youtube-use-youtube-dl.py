7077f20c64e68b12bb53e4090f310815fd1b382f
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-07-25 03:44:37 +0200

    Update youtube-use-youtube-dl.py

--- youtube-use-youtube-dl.py
+++ youtube-use-youtube-dl.py
@@ -2,5 +2,5 @@
 import os
 os.system('pip3 install youtube-dl')
 link = input('Enter the link: ')
-command = "youtube-dl {} --verbose".format(link)
+command = "youtube-dl -f best {} --verbose".format(link)
 os.system(command)
