6d09ba6cd77bc634fe4cac9840f74b6ed3a23089
Vinz2008 <clubfunreading@@gmail.com>
2021-11-14 21:44:15 +0100

    added direct youtube-dl integration in the testing cli version

--- youtube-use-youtube-dl.py
+++ youtube-use-youtube-dl.py
@@ -1,6 +1,44 @@
-
+from __future__ import unicode_literals
 import os
-os.system('pip3 install youtube-dl')
+import ffmpy
+import youtube_dl
 link = input('Enter the link: ')
-command = "youtube-dl -f best {} --verbose".format(link)
-os.system(command)
+'''
+class Logger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print("There was an error")
+        print(msg)
+'''
+def hook(d):
+    global filename
+    filename = d['filename']
+    if d['status'] == 'finished':
+        print('Converting...')
+    if d['status'] == 'downloading':
+        print(str(d['downloaded_bytes']) + 'bytes ' + '/' + str(d['total_bytes']) + 'bytes' )
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+   # 'logger': Logger(),
+    'progress_hooks': [hook],
+   # 'outtmpl': '~/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([link])
+
+filename_final = filename.replace('.m4a','.mp4')
+ff = ffmpy.FFmpeg(
+    inputs={filename: None},
+    outputs={filename_final: None}
+)
+ff.run()
+os.remove(filename)
+
+
